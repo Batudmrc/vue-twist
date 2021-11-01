ca16b13c58f5ccb177aee571fa49ddead8307a77
Batudmrc <bthn_chn_dmrc@hotmail.com>
2021-11-02 00:16:22 +0300

    Sepetim Butonu
    Sepetim butonu sepetti ürün sayısıyla ayrıldı

--- components/Header/HeaderSepet.vue
+++ components/Header/HeaderSepet.vue
@@ -5,13 +5,6 @@
         data-target="body"
         data-ready="mini-cart-ready"
         data-animate="mini-cart-animate"
-        data-type="remove"
-        class="ems-overlay mini-cart-overlay"
-      ></div>
-      <div
-        data-target="body"
-        data-ready="mini-cart-ready"
-        data-animate="mini-cart-animate"
         data-type="toggle"
         class="
           ems-cart-trigger
@@ -21,45 +14,12 @@
           align-items-center
         "
       >
-        <span class="qty shopping-cart-qty" id="lblUrunAdet">0</span
-        ><span>SEPETİM</span>
-      </div>
-      <div class="mini-cart-sub absolute transition mini-cart-content">
-        <span>
-          <div class="sub-header">
-            Sepet <span class="mcq m-lft-5"><i>0</i> Ürün</span>
-            <a
-              onclick="targetElement.clicked(this);"
-              href="javascript:void(0);"
-              data-target="body"
-              data-ready="mini-cart-ready"
-              data-animate="mini-cart-animate"
-              data-type="remove"
-              class="sub-close"
-            >
-              <svg class="icon-close">
-                <use xlink:href="/Content/Styles/base/icons.svg#close"></use>
-              </svg>
-            </a>
-          </div>
-        </span>
-
-        <div class="kutuSepet_urunYok" id="divKutuSepet_urunYok">
-          <div
-            class="
-              message-empty-card
-              align-center
-              inline-block
-              width100
-              m-top-10
-            "
-          >
-            <svg class="icon-basket m-rght-5">
-              <use xlink:href="/Content/Styles/base/icons.svg#basket"></use>
-            </svg>
-            <p>Sepetin Henüz Boş!</p>
-          </div>
-        </div>
+        <a href="/User/UserPage">
+          <span id="SepetimText">SEPETİM</span>
+        </a>
+        <a id="sepetButton" href="/User/UserPage">
+          <span class="qty shopping-cart-qty" id="lblUrunAdet">0</span>
+        </a>
       </div>
     </div>
   </div>
